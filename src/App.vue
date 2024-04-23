17db1f3110eec60d873c44cde0f18fdbf3d09316
Sergioluis Mendiola <mendiolasergioluis@gmail.com>
2024-04-23 13:19:53 -0400

    Se agregó el código para mostrar el formulario principal.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-
+import Formulario from "@/components/Formulario.vue";
 </script>
 
 <template>
-
+  <h1 class="titulo">Buscador de Clima</h1>
+  <div class="contenedor buscador-clima">
+    <Formulario/>
+  </div>
 </template>
-
 <style scoped>
 
 </style>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
+type Pais = {
+  codigo: string;
+  nombre: string;
+}
 
+const paises: Pais[] = [
+  {codigo: 'US', nombre: 'Estados Unidos'},
+  {codigo: 'MX', nombre: 'México'},
+  {codigo: 'AR', nombre: 'Argentina'},
+  {codigo: 'CL', nombre: 'Chile'},
+  {codigo: 'CO', nombre: 'Colombia'},
+  {codigo: 'CR', nombre: 'Costa Rica'},
+  {codigo: 'ES', nombre: 'España'},
+  {codigo: 'PE', nombre: 'Perú'}
+]
 </script>
 
 <template>
-
+  <form class="formulario">
+    <div class="campo">
+      <label for="ciudad">Ciudad</label>
+      <input
+          type="text"
+          id="ciudad"
+          placeholder="Ciudad"
+      >
+    </div>
+    <div class="campo">
+      <label for="pais">País</label>
+      <select id="pais">
+        <option value=""> Seleccione el País </option>
+        <option v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
+      </select>
+    </div>
+    <input type="submit" value="Consultar Clima">
+  </form>
 </template>
 
 <style scoped>
