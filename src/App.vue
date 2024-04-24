2f0ba31450e67fbf7967e434d1f88da86ea227cc
Sergioluis Mendiola <mendiolasergioluis@gmail.com>
2024-04-24 10:49:04 -0400

    Lógica del proyecto finalizada, faltan solo correcciones de tipos para antes de realizar el build

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import Formulario from "@/components/Formulario.vue";
+import Clima from "@/components/Clima.vue";
 import useClima from "@/composables/useClima";
+import Alerta from "@/components/Alerta.vue";
+import Spinner from "@/components/Spinner.vue";
+const {obtenerClima, cargando, clima, mostrarClima, error} = useClima()
 
-const {obtenerClima} = useClima()
 </script>
 
 <template>
@@ -11,6 +14,13 @@ const {obtenerClima} = useClima()
     <Formulario
         @obtener-clima="obtenerClima"
     />
+    <Spinner v-if="cargando"/>
+    <Alerta v-if="error">{{ error }}</Alerta>
+    <Clima
+        v-if="mostrarClima && !cargando"
+        :clima="clima"
+    />
+
   </div>
 </template>
 <style scoped>
--- src/composables/useClima.ts
+++ src/composables/useClima.ts
@@ -1,12 +1,58 @@
-import type {Busqueda} from "@/helpers/Types";
+import axios from 'axios';
+import {computed, ref} from 'vue'
+import { type Ref } from 'vue'
+import type {Busqueda} from '@/helpers/Types';
 
 export default function useClima() {
 
-    const obtenerClima = (busqueda: Busqueda) => {
-        // Consultar API desde aquí
+    const clima: Ref<Object> = ref({})
+    const cargando: Ref<boolean> = ref(false)
+    const error: Ref<string> = ref('')
+
+    const obtenerClima = async (busqueda: Busqueda): Promise<void> => {
+        cargando.value = true
+        // Importar la API Key
+        const {ciudad: cityName, pais: countryCode} = busqueda
+        const limit: string = '1'
+        const key: string = import.meta.env['VITE_API_KEY']
+
+        // Obtener la lat, lon
+        const url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=${limit}&appid=${key}`
+        try {
+            const {data} = await axios(url)
+            if (data.length == 0) {
+                error.value = 'Ciudad no encontrada'
+                setTimeout(() => {
+                    error.value = ''
+                }, 3000)
+            } else {
+                const {lat, lon} = data[0]
+                // Obtener el clima
+                const urlClima: string = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+                const {data: resultado} = await axios(urlClima)
+                clima.value = resultado
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            cargando.value = false
+        }
+
     }
+
+    const mostrarClima = computed((): boolean => {
+        return Object.values(clima.value).length > 0
+    })
+
+    const formatearTemperatura = (temperatura: string): string => (parseInt(temperatura) - 273.15).toFixed(1)
+
 
     return {
         obtenerClima,
+        cargando,
+        error,
+        clima,
+        mostrarClima,
+        formatearTemperatura
     }
 }
